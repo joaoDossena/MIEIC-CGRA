d8b21553303c7c390685556afed1481cbf27bb8d
FabMiguel <fabiomiguelchenhuang@gmail.com>
2020-04-02 22:15:27 +0100

    First Exercise of TP5 done

--- TP5/ex5/ShaderScene.js
+++ TP5/ex5/ShaderScene.js
@@ -68,7 +68,9 @@ class ShaderScene extends CGFscene {
 			new CGFshader(this.gl, "shaders/texture3.vert", "shaders/texture3.frag"),
 			new CGFshader(this.gl, "shaders/texture3anim.vert", "shaders/texture3anim.frag"),
 			new CGFshader(this.gl, "shaders/texture1.vert", "shaders/sepia.frag"),
-			new CGFshader(this.gl, "shaders/texture1.vert", "shaders/convolution.frag")
+			new CGFshader(this.gl, "shaders/texture1.vert", "shaders/convolution.frag"),
+			new CGFshader(this.gl, "shaders/yellow_blue.vert", "shaders/yellow_blue.frag"),
+			new CGFshader(this.gl, "shaders/texture1.vert", "shaders/grey-sepia.frag"),
 		];
 
 		// additional texture will have to be bound to texture unit 1 later, when using the shader, with "this.texture2.bind(1);"
@@ -89,7 +91,9 @@ class ShaderScene extends CGFscene {
 			'Multiple textures in VS and FS': 5,
 			'Animation example': 6,
 			'Sepia': 7,
-			'Convolution': 8
+			'Convolution': 8,
+			'Yellow Up Blue Down':9,
+			'Sepia (Grey-version)':10,
 		};
 
 		// shader code panels references
--- TP5/ex5/shaders/texture3anim.vert
+++ TP5/ex5/shaders/texture3anim.vert
@@ -14,13 +14,16 @@ uniform sampler2D uSampler2;
 uniform float normScale;
 
 void main() {
-	vec3 offset=vec3(0.0,0.0,0.0);
+	vec3 offset=vec3(0, 0, 0);
 	
 	vTextureCoord = aTextureCoord;
 
 	if (texture2D(uSampler2, vec2(0.0,0.1)+vTextureCoord).b > 0.5)
 		offset=aVertexNormal*normScale*0.1*sin(timeFactor);
 
-	gl_Position = uPMatrix * uMVMatrix * vec4(aVertexPosition+offset, 1.0);
+	vec3 backForthMotion = vec3(normScale*sin(0.1*timeFactor)	//This is used to create a back forth sin motion, 0.1 is used to slow the motion
+	+aVertexPosition.x + offset.x, aVertexPosition.y + offset.y, aVertexPosition.z + offset.z);
+
+	gl_Position = uPMatrix * uMVMatrix * vec4(backForthMotion, 1.0);
 }
 
